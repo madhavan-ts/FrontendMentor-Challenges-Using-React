21bc05206cbbbc3bb3b2dfc1940a18446f624e8e
Madhavan <mdhvntamil@gmail.com>
2024-02-03 20:39:02 +0530

    fix : unused import

--- src/component/pages/Newsletter-Sign-up/NewsletterSignUpPage.tsx
+++ src/component/pages/Newsletter-Sign-up/NewsletterSignUpPage.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import NewsletterForm from "./components/NewsletterForm";
 import SuccessMessage from "./components/SuccessMessage";
-import Attribution from "../../resuables/Attribution";
+// import Attribution from "../../resuables/Attribution";
 import "./NewsletterSignUpPage.css";
 
 function NewsletterSignUpPage() {
